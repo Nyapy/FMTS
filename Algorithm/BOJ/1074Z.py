8ade38186e1c4d542eb5a357bdeba401d34dd01e
nyapy <nyapy@naver.com>
2020-10-24 01:53:01 +0900

    201024

---
+++ Algorithm/BOJ/1074Z.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.stdin = open('1074.txt')
+
+N, r, c = map(int, input().split())
+
+y, x = r, c
+ans = -1
+def Z(x,y,z, s):
+    global ans
+    if x == 0 and y == 0:
+        ans = s
+        return
+    elif x+1 <= z and y+1 <= z :
+        Z(x,y,z//2, s)
+    else:
+        if x +1 > z and y+1 > z:
+            Z(x-z , y-z, z//2, s+ (z**2)*3)
+
+        elif x+1 > z:
+            Z(x-z, y, z//2, s+ (z**2))
+
+        elif y+1 > z:
+            Z(x,y-z, z//2, s+(z**2)*2)
+
+Z(x,y,2**N,0)
+
+print(ans)
