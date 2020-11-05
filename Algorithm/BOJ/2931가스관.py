5aa7cc2e42ac5ce56dd942a5b0985eec991a41a4
nyapy <nyapy@naver.com>
2020-11-06 01:04:16 +0900

    201106

--- Algorithm/BOJ/2931가스관.py
+++ Algorithm/BOJ/2931가스관.py
@@ -15,67 +15,61 @@ def blank(x,y):
         if blocks[y][x] == '.':
             return True
     return False
+def find():
+    for i in range(R):
+        for j in range(C):
+            if blocks[i][j] != '.':
+                if blocks[i][j] == '|':
+                    for k in [0,2]:
+                        nx,ny = j +dx[k], i +dy[k]
+                        if blank(nx,ny):
+                            return nx,ny
+                elif blocks[i][j] =='-':
+                    for k in [1,3]:
+                        nx, ny = j + dx[k], i + dy[k]
+                        if blank(nx, ny):
+                            return nx,ny
+                elif blocks[i][j] =='+':
+                    for k in [0,1,2,3]:
+                        nx, ny = j + dx[k], i + dy[k]
+                        if blank(nx, ny):
+                            return nx,ny
+                elif blocks[i][j] =='1':
+                    for k in [1,2]:
+                        nx, ny = j + dx[k], i + dy[k]
+                        if blank(nx, ny):
+                            return nx,ny
+                elif blocks[i][j] =='2':
+                    for k in [0,1]:
+                        nx, ny = j + dx[k], i + dy[k]
+                        if blank(nx, ny):
+                            return nx,ny
+                elif blocks[i][j] =='3':
+                    for k in [0,3]:
+                        nx, ny = j + dx[k], i + dy[k]
+                        if blank(nx, ny):
+                            return nx,ny
+                elif blocks[i][j] =='4':
+                    for k in [2,3]:
+                        nx, ny = j + dx[k], i + dy[k]
+                        if blank(nx, ny):
+                            return nx,ny
+X,Y = find()
 
-for i in range(R):
-    for j in range(C):
-        if blocks[i][j] != '.':
-            if blocks[i][j] == '|':
-                for k in [0,2]:
-                    nx,ny = j +dx[k], i +dy[k]
-                    if blank(nx,ny):
-                        X,Y = nx,ny
-            elif blocks[i][j] =='-':
-                for k in [1,3]:
-                    nx, ny = j + dx[k], i + dy[k]
-                    if blank(nx, ny):
-                        X,Y = nx,ny
-            elif blocks[i][j] =='+':
-                for k in [0,1,2,3]:
-                    nx, ny = j + dx[k], i + dy[k]
-                    if blank(nx, ny):
-                        X,Y = nx,ny
-            elif blocks[i][j] =='1':
-                for k in [1,2]:
-                    nx, ny = j + dx[k], i + dy[k]
-                    if blank(nx, ny):
-                        X,Y = nx,ny
-            elif blocks[i][j] =='2':
-                for k in [0,1]:
-                    nx, ny = j + dx[k], i + dy[k]
-                    if blank(nx, ny):
-                        X,Y = nx,ny
-            elif blocks[i][j] =='3':
-                for k in [0,3]:
-                    nx, ny = j + dx[k], i + dy[k]
-                    if blank(nx, ny):
-                        X,Y = nx,ny
-            elif blocks[i][j] =='4':
-                for k in [2,3]:
-                    nx, ny = j + dx[k], i + dy[k]
-                    if blank(nx, ny):
-                        X,Y = nx,ny
+down = Y+1 < R and blocks[Y+1][X] in ['|','+','2','3']
+up = 0<= Y-1 and blocks[Y-1][X] in ['|','+','1','4']
+ri = X+1 < C and blocks[Y][X+1] in ['-','+','3','4']
+le = X-1 >= 0 and blocks[Y][X-1] in ['-','+','1','2']
 
-down = Y+1 < R and blocks[Y+1][X] in ['|','+','2','3','M','Z']
-up = 0<= Y-1 and blocks[Y-1][X] in ['|','+','1','4','M','Z']
-ri = X+1 < C and blocks[Y][X+1] in ['-','+','3','4','M','Z']
-le = X-1 >= 0 and blocks[Y][X-1] in ['-','+','1','2','M','Z']
-
-ndown = Y+1 < R and blocks[Y+1][X] in ['|','+','2','3']
-nup = 0<= Y-1 and blocks[Y-1][X] in ['|','+','1','4']
-nri = X+1 < C and blocks[Y][X+1] in ['-','+','3','4']
-nle = X-1 >= 0 and blocks[Y][X-1] in ['-','+','1','2']
-
-
-
-if up and down and not nri and not nle:
+if up and down and not ri and not le:
     ans = '|'
-elif ri and le and not nup and not ndown:
+elif ri and le and not up and not down:
     ans = '-'
-elif ri and down and not nup and not nle:
+elif ri and down and not up and not le:
     ans = '1'
-elif up and ri and not nle and not ndown:
+elif up and ri and not le and not down:
     ans = '2'
-elif le and up and not nri and not ndown:
+elif le and up and not ri and not down:
     ans = '3'
 elif le and down and not nri and not nup:
     ans = '4'
