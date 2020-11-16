1d68632c662a6038eb09ce2b4fbd6c2e53317b1a
nyapy <nyapy@naver.com>
2020-11-16 23:53:12 +0900

    201116

--- Algorithm/BOJ/Silver/1331나이트투어.py
+++ Algorithm/BOJ/Silver/1331나이트투어.py
@@ -3,13 +3,44 @@ import sys
 sys.stdin = open('1331.txt')
 
 
-row, col = list(input())
-def to_int(row,col):
-    return (ord(row)-ord('A'), ord(col)-ord('1'))
+rowcol = list(input())
+def to_int(rowcol):
+    return (ord(rowcol[0])-ord('A'), ord(rowcol[1])-ord('1'))
 
+chess = [[0 for _ in range(6)] for _ in range(6)]
+row, col = to_int(rowcol)
+fr,fc = row, col
+chess[col][row] = 1
+dx = [-2,-1,1,2,2,1,-1,-2]
+dy = [-1,-2,-2,-1,1,2,2,1]
 
-row, col = to_int(row,col)
-
-print(row, col)
+ans = "Valid"
 for _ in range(35):
-    
+    nr, nc = to_int(list(input()))
+    flag = False
+    for k in range(8):
+        if [nr,nc] == [row+dx[k], col+dy[k]]:
+            flag = True
+            break
+    if flag:
+        if chess[nc][nr] == 0:
+            chess[nc][nr] = 1
+        else:
+            ans = 'Invalid'
+            break
+
+    else:
+        ans = "Invalid"
+        break
+    row, col = nr, nc
+
+last = False
+if ans =="Valid":
+    for k in range(8):
+        if [fr,fc] == [row+dx[k], col+dy[k]]:
+            last = True
+            break
+
+if not last:
+    ans = "Invalid"
+print(ans)
