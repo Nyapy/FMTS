a5b7d9a2d8816099371fabef9c7e0ba9f1e41f08
nyapy <nyapy@naver.com>
2020-12-03 23:11:23 +0900

    201203 modify

--- Algorithm/BOJ/Gold/18234당근훔쳐먹기.py
+++ Algorithm/BOJ/Gold/18234당근훔쳐먹기.py
@@ -1,17 +1,21 @@
-import sys
 
+##### 몰랐는데 그냥 input() 보다 sys.readline() 쓰는게 훨씬 빠르다.
+import sys
 sys.stdin = open('18234.txt')
 
 N,T = map(int, input().split())
 
-carrot = [list(map(int, input().split())) for _ in range(N)]
-
-carrot.sort(key= lambda x : x[1], reverse=True )
-
 ans = 0
+carrot = []
+for i in range(N):
+    c , w = map(int, sys.stdin.readline().split())
+    carrot.append(w)
+    ans += c
+
+carrot.sort(reverse=True)
 
 for i in range(N):
-    ans += carrot[i][0] + carrot[i][1]*(T-1-i)
+    ans += carrot[i]*(T-1-i)
 
 print(ans)
 
