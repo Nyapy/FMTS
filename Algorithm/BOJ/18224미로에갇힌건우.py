ce6a61cd4931e8534ff36eae06cb1637e4538a53
nyapy <nyapy@naver.com>
2020-12-06 22:47:12 +0900

    201205 success
    "Algorithm/BOJ/18224\353\257\270\353\241\234\354\227\220\352\260\207\355\236\214\352\261\264\354\232\260.py"

--- Algorithm/BOJ/18224미로에갇힌건우.py
+++ Algorithm/BOJ/18224미로에갇힌건우.py
@@ -4,30 +4,36 @@ sys.stdin = open('18224.txt')
 
 n,m = map(int, input().split())
 
-miro = [list(map(int, input().split())) for _ in range(n)]
+miro = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
 dx = [0,1,0,-1]
 dy = [-1,0,1,0]
+visited = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(2*m)]
 
 from collections import deque
 def escape():
+    visited[0][0][0] = 1
     q = deque()
-    q.append([0,0,0,1]) #x,y,이동수,낮밤
-
+    q.append([0, 0, 0, 1]) # x,y,이동수, 날짜
     while q:
-        tx,ty,move,day = q.popleft()
-        nday = day
-        if not (move+1)%m:
-            nday = (day+1)%2
+        tx, ty, move, date = q.popleft()
+        ndate = date
+        if (move+1) % (2*m):
+            nmove = move+1
+        else:
+            nmove = 0
+            ndate += 1
 
-        if day == 1:
+        if not (move//m)%2: # 낮이라면
             for k in range(4):
-                nx, ny = tx+dx[k], ty+dy[k]
-                if 0<=nx<n and 0<=ny <n:
-                    if miro[ny][nx]==0 :
-                        q.append([nx,ny,move+1,nday])
-                        if nx == n-1 and ny == n-1:
-                            return move+1
+                nx, ny = tx + dx[k], ty + dy[k]
+                if 0 <= nx < n and 0 <= ny < n:
+                    if miro[ny][nx] == 0:
+                        if visited[nmove][ny][nx] == 0:
+                            visited[nmove][ny][nx] = 1
+                            q.append([nx,ny,nmove, ndate])
+                            if nx == n - 1 and ny == n - 1:
+                                return ndate, nmove
 
         else:
             for k in range(4):
@@ -46,24 +52,23 @@ def escape():
                         break
                 if flag :
                     if miro[ny][nx] == 0 :
-                        q.append([nx,ny,move+1,nday])
-                        if nx == n-1 and ny == n-1:
-                            return move +1
-    return -1
+                        if visited[nmove][ny][nx] == 0:
+                            visited[nmove][ny][nx] =  2
+                            q.append([nx,ny,nmove, ndate])
+                            if nx == n-1 and ny == n-1:
+                                return ndate, nmove
+
+    return -1, -1
 
 if n == 1:
     print(1, "sun")
 else:
-    check = escape()
-    if check == -1:
+    adate, amove = escape()
+    if adate == -1:
         print(-1)
 
     else:
-        if (check//m)%2 :
-            print(check//m, "moon")
+        if not (amove//m)%2: # 낮이라면
+            print(adate, "sun")
         else:
-            ans = check//m
-            if ans == 0:
-                ans = 1
-            print(ans, "sun")
-
+            print(adate, "moon")
