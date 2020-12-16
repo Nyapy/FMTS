424a15cf0c14a8aab99af4b20aeac7729f8ed954
nyapy <nyapy@naver.com>
2020-12-16 23:56:20 +0900

    201216

--- Algorithm/BOJ/Gold/2151거울설치.py
+++ Algorithm/BOJ/Gold/2151거울설치.py
@@ -19,22 +19,57 @@ for i in range(N):
 sx,sy = moon[0]
 ex,ey = moon[1]
 
-from collections import deque
-from itertools import combinations
-
-
-
-q.append([sx,sy])
-
-for i in range(len(candi)+1):
-    for comb in combinations(candi, i):
-        for x,y in comb:
-            house[y][x] = 'm'
-
-        for x,y in comb:
-            house[y][x] = '.'
 
 def bfs():
     visited = [[0 for _ in range(N)] for _ in range(N)]
     q = deque()
     visited[sy][sx] = 1
+    q.append([sx, sy, 0])
+    q.append([sx, sy, 1])
+    q.append([sx, sy, 2])
+    q.append([sx, sy, 3])
+
+    while q:
+        tx, ty, tk = q.pop()
+        flag = 1
+        while flag:
+            nx, ny = tx + dx[tk], ty + dy[tk]
+
+            if 0 <= nx < N and 0 <= ny < N and visited[ny][nx] == 0:
+                tx, ty = nx, ny
+
+            else:
+                flag = 0
+                break
+
+            if house[ty][tx] == 'm':
+                flag = 0
+                q.append([tx, ty, (tk + 1) % 4])
+                q.append([tx, ty, (tk + 3) % 4])
+
+            if tx == ex and ty == ey:
+                return 1
+    return 0
+
+
+from collections import deque
+from itertools import combinations
+
+ans = 0
+dx, dy = [1,0,-1,0],[0, 1, 0 ,-1]
+for i in range(len(candi)+1):
+    if ans :
+        break
+    for comb in combinations(candi, i):
+        for x,y in comb:
+            house[y][x] = 'm'
+        go = bfs()
+
+        if go:
+            ans = i
+            break
+        for x,y in comb:
+            house[y][x] = '.'
+
+print(ans)
+
