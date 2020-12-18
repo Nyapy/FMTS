781a5982e78bd098ec637ea0ba185165b925aa44
nyapy <nyapy@naver.com>
2020-12-18 23:43:54 +0900

    201218

--- Algorithm/BOJ/Gold/1034램프.py
+++ Algorithm/BOJ/Gold/1034램프.py
@@ -8,31 +8,32 @@ lamp = [list(map(int, input())) for _ in range(N)]
 
 K = int(input())
 ans = 0
-from copy import deepcopy
-def dfs(s,lampt, cnt ):
+
+def dfs(s, cnt ):
     global ans
     if ans == N:
         return
     if cnt == K:
         state = 0
         for a in range(N):
-            if not 0 in lampt[a]:
+            if not 0 in lamp[a]:
                 state += 1
         if ans < state:
             ans = state
         return
 
-    tem = deepcopy(lampt)
+    tem = []
     for i in range(s,M):
         for j in range(N):
-            if lampt[j][i]:
-                lampt[j][i] = 0
+            tem.append(lamp[j][i])
+            if lamp[j][i]:
+                lamp[j][i] = 0
             else:
                 lamp[j][i] = 1
-        dfs(i, lampt, cnt+1)
+        dfs(i, cnt+1)
 
         for j in range(N):
-            lamp[j][i] = tem[j][i]
+            lamp[j][i] = tem[j]
 
-dfs(0,lamp,0)
+dfs(0,0)
 print(ans)
