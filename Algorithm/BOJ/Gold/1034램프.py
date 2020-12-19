e065d64b71135886ba30482b875b828b7610b3cc
nyapy <nyapy@naver.com>
2020-12-19 22:45:51 +0900

    201219

--- Algorithm/BOJ/Gold/1034램프.py
+++ Algorithm/BOJ/Gold/1034램프.py
@@ -9,31 +9,66 @@ lamp = [list(map(int, input())) for _ in range(N)]
 K = int(input())
 ans = 0
 
-def dfs(s, cnt ):
+def swich():
     global ans
-    if ans == N:
-        return
-    if cnt == K:
-        state = 0
-        for a in range(N):
-            if not 0 in lamp[a]:
-                state += 1
-        if ans < state:
-            ans = state
-        return
 
-    tem = []
-    for i in range(s,M):
-        for j in range(N):
-            tem.append(lamp[j][i])
-            if lamp[j][i]:
-                lamp[j][i] = 0
-            else:
-                lamp[j][i] = 1
-        dfs(i, cnt+1)
+    check = [0 for _ in range(N)]
 
-        for j in range(N):
-            lamp[j][i] = tem[j]
+    for i in range(N):
+        if check[i]:
+            pass
+        else:
+            check[i] = 1
 
-dfs(0,0)
+            zero = 0
+            for j in range(M):
+                if lamp[i][j] == 0:
+                    zero += 1
+
+            if zero <= K and K%2 == zero%2:
+                cnt = 1
+                for k in range(i+1,N):
+                    if lamp[i] == lamp[k]:
+                        cnt += 1
+                        check[k] = 1
+                if cnt > ans:
+                    ans = cnt
+
+
+
+
+# def dfs(s, cnt ):
+#     global ans
+#     if s== M :
+#         return
+#     if ans == N:
+#         return
+#     if cnt == K:
+#         state = 0
+#         for a in range(N):
+#             if not 0 in lamp[a]:
+#                 state += 1
+#         if ans < state:
+#             ans = state
+#         return
+#
+#     dfs(s + 1, cnt)
+#
+#     tem = []
+#     for i in range(N):
+#         tem.append(lamp[i][s])
+#         if lamp[i][s]:
+#             lamp[i][s] = 0
+#         else:
+#             lamp[i][s] = 1
+#
+#
+#     dfs(s,cnt+1)
+#
+#     for i in range(N):
+#         lamp[i][s] = tem[i]
+#
+# dfs(0,0)
+
+swich()
 print(ans)
