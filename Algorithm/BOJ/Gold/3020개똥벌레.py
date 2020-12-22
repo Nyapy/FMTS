1efb3b2a9581bde41cf19eacbbd7674a86746070
nyapy <nyapy@naver.com>
2020-12-22 23:40:52 +0900

    201222이진탐색

--- Algorithm/BOJ/Gold/3020개똥벌레.py
+++ Algorithm/BOJ/Gold/3020개똥벌레.py
@@ -11,4 +11,21 @@ for _ in range(N//2):
     down.append(int(input()))
     up.append(int(input()))
 
+up.sort()
+down.sort()
 print(up, down)
+l = 0
+r = N//2-1
+m = (N//2)//2
+
+i = 4
+while l != r:
+    if up[m] > i:
+        r = m -1
+
+    else:
+        l = m+1
+
+    m = l+(r - l) // 2
+
+print(l)
