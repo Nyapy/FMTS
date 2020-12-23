b9b1e9b111085e6e0cd35b87cb73ac9a1ffd0110
nyapy <nyapy@naver.com>
2020-12-23 23:27:26 +0900

    201223

--- Algorithm/BOJ/Gold/3020개똥벌레.py
+++ Algorithm/BOJ/Gold/3020개똥벌레.py
@@ -8,24 +8,50 @@ up = []
 down = []
 cnt = [0 for _ in range(H)]
 for _ in range(N//2):
-    down.append(int(input()))
-    up.append(int(input()))
+    down.append(int(sys.stdin.readline()))
+    up.append(int(sys.stdin.readline()))
 
 up.sort()
 down.sort()
-print(up, down)
-l = 0
-r = N//2-1
-m = (N//2)//2
 
-i = 4
-while l != r:
-    if up[m] > i:
-        r = m -1
+for i in range(H):
+    l = 0
+    r = N // 2
+    m = -1
+    while l < r:
+        m = (r+l)//2
+        if down[m] >= i+1:
+            r = m
+        elif down[m] < i+1:
+            l = m+1
+    cnt[i] = N//2-l
+    if N//2-l == 0:
+        for j in range(i+1,H):
+            cnt[j] = 0
+        break
 
-    else:
-        l = m+1
+print(cnt)
+for i in range(H):
+    l = 0
+    r = N // 2
+    m = -1
+    di = H-i
+    while l < r:
+        m = (r+l)//2
+        if up[m] >= di:
+            r = m
 
-    m = l+(r - l) // 2
+        elif up[m] < di:
+            l = m+1
+    cnt[i] += N//2-l
+    if N//2-l == N//2:
+        for j in range(i+1,H):
+            cnt[j] += N//2
+        break
+print(cnt)
+ans = 0
+for i in range(H):
+    if cnt[i] == min(cnt):
+        ans += 1
 
-print(l)
+print(min(cnt), ans)
