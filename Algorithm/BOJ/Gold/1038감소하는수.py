604f9a0344def5d780b91d37a2338d8550192a70
nyapy <nyapy@naver.com>
2020-12-30 00:16:07 +0900

    201229
    Former-commit-id: e57efb335ffc7c2fdb75cadf19d00f56cc974298

--- Algorithm/BOJ/Gold/1038감소하는수.py
+++ Algorithm/BOJ/Gold/1038감소하는수.py
@@ -5,18 +5,18 @@ sys.stdin = open('1038.txt')
 N = int(input())
 
 candi = range(10)
-
-print(candi)
 cnt = -1
-
+case = []
 def descent(k, n, a):
     global cnt
     if a == 11:
         return
     if k == n:
         cnt += 1
-        print(cnt)
-        print(number)
+        num = ''
+        for t in range(len(number)-1,-1,-1):
+            num += str(number[t])
+        case.append(int(num))
         return
 
     else:
@@ -24,7 +24,13 @@ def descent(k, n, a):
             number[k] = candi[q]
             descent(k+1,n, q+1)
 
-for i in range(1,10):
-    number = [-1] *i
-    descent(0,i,0)
+
+if N >= 1023:
+    print(-1)
+else:
+    for i in range(1,11):
+        number = [-1] *i
+        descent(0,i,0)
+    case.sort()
+    print(case[N])
 
