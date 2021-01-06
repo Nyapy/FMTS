2c7fbd6bc9355526e05c7a2b0d5a60ab44178f3b
nyapy <nyapy@naver.com>
2021-01-06 23:51:40 +0900

    210106

--- Algorithm/JAVA/HWjava/src/day210104.java
+++ Algorithm/JAVA/HWjava/src/day210104.java
@@ -28,7 +28,49 @@ public class day210104 {
 		System.out.println(0011 & 1010);
 		System.out.println(0010);
 		//에러 System.out.println(0101 && 0101);
+		int s1 = 10;
+		int s2 = 10;
+		System.out.println(s1==s2);
+		
+		int a = 2;
+		int b = 5;
+		int c = a|b;
+		int d = a&b;
+		int e = a^b;
+		int i, j;
+		i = a <<2 ;
+		j = b >> 2;
+		System.out.println("a = "+ a);
+		System.out.println("b = "+ b);
+		System.out.println("a|b = " + c);
+		System.out.println("a&b= " + d);
+		System.out.println("a ^ b = " + e);
+		System.out.println("a << 2 = " + i);
+		System.out.println("b >> 2 = " + j );
 				
+		System.out.println("-2 >> 2 = " + (-5>>2) );
+		String a1 = "aaa";
+		String a2 = "aaa";
+		if (a1==a2) {
+			System.out.println("저거 두개 같음");
+		}
+		else {
+			System.out.println("아냐 다름");
+		}
+		
+		int scoreList[] ;
+		scoreList = new int[10];
+		
+		scoreList[0] = 100;
+		scoreList[1] = 80;
+		scoreList[2] = 90;
+		scoreList[3] = 70;
+		scoreList[4] = 85;
+		System.out.println(scoreList.length);
+		System.out.println(scoreList[9]);
+		
+		int AA[] = {45, 10, 80, 100, 60};
+		System.out.println(AA[3]);
 	}
 
 }
