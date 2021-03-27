00b6f8efa45c5468a4f3fe826ea60b7f43c44532
nyapy <nyapy@naver.com>
2021-03-27 23:54:26 +0900

    2243 사탕상자

--- Algorithm/C/algo/algo/1753최단경로.cpp
+++ Algorithm/C/algo/algo/1753최단경로.cpp
@@ -1,69 +1,69 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-//#define eq(x) (x)==INF
-#define eq(x) if((x) == INF)
-#define INF 20000001
-
-int V, E, K;
-using namespace std;
-int u, v, w;
-pair<int, int> node;
-
-struct cmp {
-	bool operator()(pair<int, int> a, pair<int, int> b){
-		return a.second > b.second;
-	}
-};
-
-priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> go;
-
-vector<pair<int, int>> path[20001];
-
-int weight[20001];
-
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	freopen("1753.txt", "r", stdin);
-	cin >> V >> E >> K;
-
-	for (int i = 0; i < E; i++) {
-		cin >> u >> v >> w;
-		node.first = v;
-		node.second = w;
-		path[u].push_back(node);
-	}
-
-	for (int i = 1; i <= V; i++) weight[i] = INF;
-	
-	weight[K] = 0;
-	go.push(make_pair(K, 0));
-	
-	while (!go.empty()) {
-		pair<int, int> sel = go.top();
-		go.pop();
-		int num = sel.first;
-		int cost = sel.second;
-
-		for (int i = 0; i < path[num].size(); i++) {
-			if (cost + path[num][i].second < weight[path[num][i].first]) {
-				weight[path[num][i].first] = cost + path[num][i].second;
-				go.push(make_pair(path[num][i].first, cost + path[num][i].second));
-			}
-		}
-
-		path[sel.first];
-	}
-	
-	for (int i = 1; i <= V; i++) {
-		eq(weight[i]) {
-			cout << "INF\n";
-			continue;
-		}
-		cout << weight[i] << "\n";
-	}
-	return 0;
-
-}
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+////#define eq(x) (x)==INF
+//#define eq(x) if((x) == INF)
+//#define INF 20000001
+//
+//int V, E, K;
+//using namespace std;
+//int u, v, w;
+//pair<int, int> node;
+//
+//struct cmp {
+//	bool operator()(pair<int, int> a, pair<int, int> b){
+//		return a.second > b.second;
+//	}
+//};
+//
+//priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> go;
+//
+//vector<pair<int, int>> path[20001];
+//
+//int weight[20001];
+//
+//int main(void) {
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	freopen("1753.txt", "r", stdin);
+//	cin >> V >> E >> K;
+//
+//	for (int i = 0; i < E; i++) {
+//		cin >> u >> v >> w;
+//		node.first = v;
+//		node.second = w;
+//		path[u].push_back(node);
+//	}
+//
+//	for (int i = 1; i <= V; i++) weight[i] = INF;
+//	
+//	weight[K] = 0;
+//	go.push(make_pair(K, 0));
+//	
+//	while (!go.empty()) {
+//		pair<int, int> sel = go.top();
+//		go.pop();
+//		int num = sel.first;
+//		int cost = sel.second;
+//
+//		for (int i = 0; i < path[num].size(); i++) {
+//			if (cost + path[num][i].second < weight[path[num][i].first]) {
+//				weight[path[num][i].first] = cost + path[num][i].second;
+//				go.push(make_pair(path[num][i].first, cost + path[num][i].second));
+//			}
+//		}
+//
+//		path[sel.first];
+//	}
+//	
+//	for (int i = 1; i <= V; i++) {
+//		eq(weight[i]) {
+//			cout << "INF\n";
+//			continue;
+//		}
+//		cout << weight[i] << "\n";
+//	}
+//	return 0;
+//
+//}
---
+++ Algorithm/C/algo/algo/2243사탕상자.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+int N, A, B, C, tem;
+int ans = 0;
+
+int tree[4000001];
+
+using namespace std;
+
+int put_candy(int num, int start, int end, int taste, int cnum) {
+
+	if (start > taste || end < taste) return 0;
+
+	if (start == end) {
+		tree[num] +=cnum;
+		return tree[num];
+	}
+
+	put_candy(num * 2, start, (start + end) / 2, taste, cnum);
+	put_candy(num * 2 + 1, (start + end) / 2 + 1, end, taste, cnum);
+	tree[num] += cnum;
+	return tree[num] ;
+}
+
+int take_candy(int num, int start, int end, int taste) {
+	if (start > taste) return 0;
+
+	if (start == end && start == taste) {
+		tree[num] --;
+		tem = num;
+		return tree[num];
+	}
+	if (taste > end) return tree[num];
+
+	int left = take_candy(num * 2, start, (start + end) / 2, taste);
+	int right = take_candy(num * 2 + 1, (start + end) / 2 + 1, end, taste);
+	tree[num] = left + right;
+	return tree[num];
+}
+
+int main(void) {
+	freopen("2243.txt", "r", stdin);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> A;
+		
+		if (A == 1) {
+
+			cin >> B;
+			int aa = take_candy(1, 1, 1000000, B);
+			int ab = tree[tem];
+			cout << aa-tree[tem]<<"\n";
+		}
+		else if (A == 2) {
+			cin >> B >> C;
+			put_candy(1, 1, 1000000, B, C);
+		}
+	}
+
+	return 0;
+}
